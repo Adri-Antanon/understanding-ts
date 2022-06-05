133f95870ba59ce81e01cb9b7f9dc88b8fdb75f9
Adrian Antanon <adrianantanon@gmail.com>
2022-06-05 23:06:01 +0200

    feat: type aliases

--- app.ts
+++ app.ts
@@ -1,13 +1,25 @@
-type Combine = number | string;
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-text';
 
-const combine = (n1: Combine, n2: Combine): Combine => {
-  let result: Combine;
-  if (typeof n1 === 'number' && typeof n2 === 'number') {
-    result = n1 + n2;
+const combine = (
+  n1: Combinable,
+  n2: Combinable,
+  resultConversion: ConversionDescriptor,
+): Combinable => {
+  let result: Combinable;
+  if (
+    (typeof n1 === 'number' && typeof n2 === 'number') ||
+    resultConversion === 'as-number'
+  ) {
+    result = +n1 + +n2;
   } else {
     result = n1.toString() + n2.toString();
   }
   return result;
 };
 
-const combinedAges = combine(30, 12);
+const combinedAges = combine(30, 12, 'as-number');
+console.log(combinedAges);
+
+const combinedNames = combine('Adri', 'Vasile', 'as-text');
+console.log(combinedNames);
