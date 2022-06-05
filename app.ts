9ef3386a38fee42a5909d0e6ea6c2242dc13b32e
Adrian Antanon <adrianantanon@gmail.com>
2022-06-05 17:38:34 +0200

    feat: type string, number & boolean

--- app.ts
+++ app.ts
@@ -1,1 +1,15 @@
-console.log('Your code goes here...');
+const add = (n1: number, n2: number, printResult: boolean, resultPhrase: string): number => {
+    if (printResult) {
+        console.log(resultPhrase, n1 + n2);
+    } else {
+        return n1 + n2;
+    }
+
+}
+
+const num1 = 10;
+const num2 = 2.5;
+const printResult = true
+const resultPhrase = 'Result is: ';
+
+const result = add(num1, num2, printResult, resultPhrase);
